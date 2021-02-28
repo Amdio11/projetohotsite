69e8736cc65e4668892931c11f4c83ad5c27cd71
Amdio Rocha <amdio0811@gmail.com>
2021-02-28 04:40:12 -0300

    Incluido try catch e alertas via Jquery js

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Hotsite.Models;
+using MySql.Data.MySqlClient;
 
 namespace Hotsite.Controllers
 {
@@ -20,16 +21,33 @@ namespace Hotsite.Controllers
 
         public IActionResult Index()
         {
+            _logger.LogWarning("isso é um alerta!!");
             return View();
         }
 
         [HttpPost]
         public IActionResult Cadastrar(Interesse cad)
         {
+            try{
             DatabaseService dbs = new DatabaseService();
             dbs.CadastraInteresse(cad);
-            return View("Index",cad);
+             return Json(new {Status = "OK"});   
+            }
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message);
+                return Json(new {Status = "ERR", Mensagem = "Deu ruim heim...falhouuuuu o banco de dados !!!"});
+                //throw new Exception("A coisa ta feia heim...Deu ruim de novo!!!"); //uso para fazer o comentario se nao usar js
+            }
+            
+            //return View("Index",cad);
         }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
 
     }
 }
