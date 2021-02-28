6868bb1debf1851b8172627edfad8f6ca9b7a3db
Amdio Rocha <amdio0811@gmail.com>
2021-02-28 04:59:31 -0300

    Criacao dos codigos js para mensagem de erro e de sucesso do cadastro.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,42 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function CadastraInteresse()
+{
+  var parametros = {
+    nome: $("#txtNome").val(),
+    email: $("#txtEmail").val(),
+    mensagem: $("#txtMensagem").val()
+  };
+  $.post("/Home/Cadastrar", parametros)
+      .done(function(data){
+        if(data.status == "OK")
+        {
+          $("#formCadastro").after("<h2>Ai sim heim, deu bom yuhuuuu...</h2>");
+          $("#formCadastro").hide();
+          $("#agenda").modal();
+        }
+        else
+        {
+          if(data.status == "ERR")
+          {
+            $("#formCadastro").after("<h2>" + data.mensagem+"</h2>");
+          }
+        }
+        
+        
+  })
+      .fail(function(){
+        alert("Deu ruim heim !!!")
+      });
+
+}
+$(document).ready(
+    function(){
+      $("#formCadastro").submit(function(e){
+        e.preventDefault();
+        CadastraInteresse();
+    })
+   }
+  );
